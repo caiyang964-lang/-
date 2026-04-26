a4ea1908599caac4d83750592efe7063187de9ea
caiyang964-lang <caiyang964@gmail.com>
2026-04-26 17:59:21 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Gallery from './pages/Gallery';
@@ -8,7 +8,8 @@ import About from './pages/About';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    // 关键修复：改用 HashRouter。这会在网址里加上 # 符号，防止 GitHub Pages 强制寻找物理文件
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
@@ -19,6 +20,6 @@ export default function App() {
           <Route path="upload" element={<Upload />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
